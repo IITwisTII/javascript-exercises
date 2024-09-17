aff47fb5e71f454a9fa5512c2a1b0a52731ed45b
IITwisTII <ali_kashash@hotmail.co.uk>
2024-09-17 17:23:55 +0200

    completed

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,30 @@
-const sumAll = function() {
+const sumAll = function(x, y) {
+    if (x < 0 || y < 0) {
+        return "ERROR";
+    } else if (Number.isNaN(x) || Number.isNaN(y)) {
+        return "ERROR";
+    } else if (!Number.isInteger(x) || !Number.isInteger(y)) {
+        return "ERROR";
+    } else {
+        let sum = 0;
+        let lowernum;
+        let highernum;
+        
+        if (x < y) {
+            lowernum = x;
+            highernum = y;
+        } else if (y < x) {
+            lowernum = y;
+            highernum = x;
+        }
 
+        for (let i = lowernum; i <= highernum; i++)
+            sum += i;
+        return sum;
+    }
 };
+
+console.log(sumAll(2.5, 4000))
 
 // Do not edit below this line
 module.exports = sumAll;
